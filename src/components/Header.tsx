7d06e945ac8427d6336a1fc1bfd3491716c3086f
CristopherMartarello <crisrossi1313@gmail.com>
2025-10-07 15:50:22 -0300

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import GithubLogo from "../assets/github-logo.png";
+
+const Header = () => {
+  return (
+    <header className="bg-header flex h-18 w-full items-center px-7">
+      <div className="flex w-full items-center gap-5 text-white">
+        <img src={GithubLogo} alt="GitHub logo" className="h-[30px] w-auto" />
+        <span className="text-2xl font-normal">/</span>
+        <span className="text-base font-light">Profile</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,20 @@ import RepoList from "@/components/RepoList";
 import ProfileCard from "../components/ProfileCard";
 import SearchBar from "../components/SearchBar";
 import Tabs from "../components/Tabs";
+import Header from "@/components/Header";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start bg-neutral-50 p-7 text-neutral-400">
-      <ProfileCard />
-      <div className="mt-8 flex w-full">
-        <Tabs />
+    <div className="flex min-h-screen flex-col bg-neutral-50 text-neutral-400">
+      <Header />
+      <div className="flex flex-col items-center justify-start p-7">
+        <ProfileCard />
+        <div className="mt-8 flex w-full">
+          <Tabs />
+        </div>
+        <SearchBar />
+        <RepoList />
       </div>
-      <SearchBar />
-      <RepoList />
     </div>
   );
 };
